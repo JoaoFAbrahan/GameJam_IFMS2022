7665d966d01b9cd2c6ecb2df4bc994323f1ba49a
gabrielucido <gabrielucido@gmail.com>
2022-10-01 15:53:00 -0400

    Feat: Basic movement

--- Assets/Project Assets/Scripts/Player/CODE_CharacterPlayer.cs
+++ Assets/Project Assets/Scripts/Player/CODE_CharacterPlayer.cs
@@ -6,23 +6,24 @@ namespace CHARACTERS
 {
     public class CODE_CharacterPlayer : CODE_PauseFunction, ICODE_CharacterMovimentation
     {
-        // Class attributes
-
-
+        public float speed = 1;
+        private float _speedScale = 1000;
 
         private void Start()
         {
-            
+            speed *= _speedScale;
         }
 
         private void Update()
         {
-            
+            this.Movimentation();
         }
 
         public void Movimentation()
         {
-
+            float vertical_movement = Input.GetAxis("Vertical");
+            float horizontal_movement = Input.GetAxis("Horizontal");
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal_movement * speed * Time.deltaTime, vertical_movement * speed * Time.deltaTime);
         }
 
         private void DeathCondition()
