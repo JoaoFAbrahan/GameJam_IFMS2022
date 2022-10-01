8eb855c206919fc6655d0f79ed35fee77437e0ac
João Felipe Abrahan <joaofelipeabrahan@hotmail.com>
2022-10-01 15:02:06 -0400

    Classes bases de implementação de Personagens
    Adicionada classes responsáveis pela movimentação dos personagens

---
+++ Assets/Project Assets/Actors/Characters/CODE_PauseFunction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CHARACTERS
+{
+    public class CODE_PauseFunction : MonoBehaviour
+    {
+        public void PauseGame()
+        {
+
+        }
+    }
+}
---
+++ Assets/Project Assets/Actors/Characters/Player/CODE_CharacterPlayer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CHARACTERS
+{
+    public class CODE_CharacterPlayer : CODE_PauseFunction, ICODE_CharacterMovimentation
+    {
+        // Class attributes
+
+
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        public void Movimentation()
+        {
+
+        }
+
+        private void DeathCondition()
+        {
+
+        }
+
+        public void PausePlayer()
+        {
+
+        }
+
+    }
+}
